54afe6c8ba57c2ed597591841a1c81094f2fbb11
Lewis Baker <lewissbaker@gmail.com>
2013-10-18 21:48:36 +1030

    Document some methods in sequence_range class.

--- include/disruptorplus/sequence_range.hpp
+++ include/disruptorplus/sequence_range.hpp
@@ -5,6 +5,10 @@
 
 namespace disruptorplus
 {
+    // A sequence range represents a contiguous range of sequence numbers.
+    //
+    // The range of sequence numbers may overflow the underlying integer
+    // storage type in which case the values wrap back around to zero.
     class sequence_range
     {
     public:
@@ -15,17 +19,25 @@ namespace disruptorplus
         , m_size(0)
         {}
         
+        // Construct to a range of 'size' sequence numbers starting at 'first'.
         sequence_range(sequence_t first, size_t size)
         : m_first(first)
         , m_size(size)
         {}
 
+        // Number of sequence numbers in this range.
         size_t size() const { return m_size; }
         
+        // The first sequence number in the range.
         sequence_t first() const { return m_first; }
+        
+        // The last sequence in the range.
         sequence_t last() const { return end() - 1; }
+        
+        // One-past the last sequence number in the range.
         sequence_t end() const { return static_cast<sequence_t>(m_first + m_size); }
         
+        // Lookup the nth sequence number in the range.
         sequence_t operator[](size_t index) const { return static_cast<sequence_t>(m_first + index); }
         
     private:
