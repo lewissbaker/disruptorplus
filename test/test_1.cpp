687f1440f4ec04c6a1213d003da40a8a3525c5cd
Lewis Baker <lewissbaker@gmail.com>
2013-10-19 15:31:49 +1030

    Now output more statistics in the test program.
    - Includes average number of items per second.
    - Includes most frequently occuring reader batch sizes.

--- test/test_1.cpp
+++ test/test_1.cpp
@@ -25,10 +25,11 @@ int main(int argc, char* argv[])
     claimStrategy.add_claim_barrier(finishedReading);
     ring_buffer<message> buffer(bufferSize);
     
-    uint64_t result;
+    std::vector<size_t> readerBatchSizes(bufferSize, 0);
     
     auto start = std::chrono::high_resolution_clock::now();
-    
+
+    uint64_t result;
     std::thread reader([&]() {
         bool exit = false;
         uint64_t sum = 0;
@@ -37,6 +38,7 @@ int main(int argc, char* argv[])
         {
             sequence_t available = claimStrategy.wait_until_published(nextToRead);
             assert(difference(available, nextToRead) >= 0);
+            ++readerBatchSizes[difference(available, nextToRead)];
             do
             {
                 auto& message = buffer[nextToRead];
@@ -100,7 +102,30 @@ int main(int argc, char* argv[])
     std::cout << result << "\n"
               << durMS.count() << "ms total time\n"
               << nsPerItem.count() << "ns per item (avg)\n"
+              << (1000000000 / nsPerItem.count()) << " items per second (avg)\n"
               << std::flush;
+
+    std::vector<std::pair<size_t, size_t>> sortedBatchSizes;
+    for (size_t i = 0; i < readerBatchSizes.size(); ++i)
+    {
+        if (readerBatchSizes[i] != 0)
+        {
+            sortedBatchSizes.push_back(std::make_pair(readerBatchSizes[i] * (i + 1), (i + 1)));
+        }
+    }
+    std::sort(sortedBatchSizes.rbegin(), sortedBatchSizes.rend());
+
+    std::cout << "Reader batch sizes:\n";
+    for (size_t i = 0; i < 20 && i < sortedBatchSizes.size(); ++i)
+    {
+        size_t batchSize = sortedBatchSizes[i].second;
+        size_t batchItemCount = sortedBatchSizes[i].first;
+        size_t percentage = (100 * batchItemCount) / itemCount;
+        std::cout << "#" << (i + 1) << ": " << batchSize
+                  << " item batch, " << percentage << "%, "
+                  << (sortedBatchSizes[i].first / sortedBatchSizes[i].second) << " times\n";
+    }
+    std::cout << std::flush;
     
     return 0;
 }
